f349039b1b7d2f6ace4d0e1f2dea5885a0dcfb36
darkfr3ak <info@darkfr3ak.de>
2014-03-28 20:23:24 +0100

    added ` to some classes

--- application/classes/Register.class.php
+++ application/classes/Register.class.php
@@ -113,13 +113,13 @@ class Register extends Base{
                 $this->user_password_hash = password_hash($this->user_password, PASSWORD_DEFAULT);
 
                 // check if user already exists
-                $query_check_user_name = $this->db_connection->query("SELECT * FROM ".Conf::$DB_PREFIX."core_users WHERE user_name = '" . $this->user_name . "';");
+                $query_check_user_name = $this->db_connection->query("SELECT * FROM `".Conf::$DB_PREFIX."core_users` WHERE user_name = '" . $this->user_name . "';");
 
                 if ($query_check_user_name->num_rows == 1) {
                     $this->errors[] = "Sorry, that user name is already taken. Please choose another one.";
                 } else {
                     // write new users data into database
-                    $query_new_user_insert = $this->db_connection->query("INSERT INTO ".Conf::$DB_PREFIX."core_users (user_name, user_password_hash, user_email) VALUES('" . $this->user_name . "', '" . $this->user_password_hash . "', '" . $this->user_email . "');");
+                    $query_new_user_insert = $this->db_connection->query("INSERT INTO `".Conf::$DB_PREFIX."core_users` (user_name, user_password_hash, user_email) VALUES('" . $this->user_name . "', '" . $this->user_password_hash . "', '" . $this->user_email . "');");
 
                     if ($query_new_user_insert) {
                         $this->messages[] = "Your account has been created successfully. You can now <a href='index.php?app=login'>log in</a>.";
